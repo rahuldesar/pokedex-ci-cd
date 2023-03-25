76a5916bdfb327793bdd4568b1eb2871a24b25be
Rahul Desar <desarrahul@gmail.com>
2023-03-25 16:58:27 +0545

    failed?

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,6 @@ const App = () => {
       <Switch>
         <Route exact path="/">
           <PokemonList pokemonList={pokemonList} />
-          Should work now.
         </Route>
         <Route
           path="/pokemon/:name"
